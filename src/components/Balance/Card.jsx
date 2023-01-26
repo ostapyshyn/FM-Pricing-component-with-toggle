5394ee5c21c01fc98ba2fbe4fd9d193c6dbeec6f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-01-26 23:57:52 +0200

    Fix paddings

--- src/components/Balance/Card.jsx
+++ src/components/Balance/Card.jsx
@@ -2,33 +2,45 @@ import React from 'react'
 import Button from '../Button'
 import styles from './Card.module.scss'
 
-export default function Card({ title, price, storage, users, limit, isPro }) {
+export default function Card({ title, price, storage, users, limit, isPro, isLeft }) {
+  const whiteColorStyle = {
+    color: '#ffffff',
+  }
   return (
     <section
       className={styles.card}
-      style={isPro && { background: 'linear-gradient(135deg, #a2a7f0 0%, #696edd 100%)' }}>
-      <h2 className={styles.title} style={isPro && { color: '#ffffff' }}>
+      style={
+        (isPro && {
+          background: 'linear-gradient(135deg, #a2a7f0 0%, #696edd 100%)',
+          paddingBlock: '54.5px',
+          borderRadius: '10px',
+        },
+        isLeft && {
+          borderRadius: '10px 10px 0 0',
+        })
+      }>
+      <h2 className={styles.title} style={isPro && whiteColorStyle}>
         {title}
       </h2>
       <div className={styles.cost}>
-        <span className={styles.dollar} style={isPro && { color: '#ffffff' }}>
+        <span className={styles.dollar} style={isPro && whiteColorStyle}>
           $
         </span>
-        <p className={styles.price} style={isPro && { color: '#ffffff' }}>
+        <p className={styles.price} style={isPro && whiteColorStyle}>
           {price}
         </p>
       </div>
 
       <hr />
-      <p className={styles.storage} style={isPro && { color: '#ffffff' }}>
+      <p className={styles.storage} style={isPro && whiteColorStyle}>
         {storage} Storage
       </p>
       <hr />
-      <p className={styles.storage} style={isPro && { color: '#ffffff' }}>
+      <p className={styles.storage} style={isPro && whiteColorStyle}>
         {users} Users Allowed
       </p>
       <hr />
-      <p className={styles.storage} style={isPro && { color: '#ffffff' }}>
+      <p className={styles.storage} style={isPro && whiteColorStyle}>
         Send up to {limit} GB
       </p>
       <hr />
